a9717e4d8840f808c008ebf7688d7d1e967c7071
Arenui Anderson <arenui.anderson@mx.com>
2022-12-06 11:24:52 -0700

    give it up for day 6

---
+++ day6/day6.py
@@ -0,0 +1,39 @@
+##### DAY 6 #####
+
+# Input Processing
+with open(f"day6/input.txt") as f:
+    input_string = f.readlines()[0]
+
+# Star 1
+def find_packet_start(datastream: str) -> int:
+    buffer = []
+    for pos in range(1, len(datastream) + 1):
+        if (
+            datastream[pos - 1] not in buffer
+            and len(buffer) == 3
+            and len(set(buffer)) == len(buffer)
+        ):
+            return pos
+        elif len(buffer) == 3:
+            buffer.pop(0)
+        buffer.append(datastream[pos - 1])
+
+
+print("Star 1:", find_packet_start(datastream=input_string))
+
+# Star 2
+def find_message_start(datastream: str) -> int:
+    buffer = []
+    for pos in range(1, len(datastream) + 1):
+        if (
+            datastream[pos - 1] not in buffer
+            and len(buffer) == 13
+            and len(set(buffer)) == len(buffer)
+        ):
+            return pos
+        elif len(buffer) == 13:
+            buffer.pop(0)
+        buffer.append(datastream[pos - 1])
+
+
+print("Star 2:", find_message_start(datastream=input_string))
